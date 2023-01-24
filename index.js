403030795d81921234a9480ff57694a27c513c2f
jackymomomo <jacklloydrobinson@gmail.com>
2023-01-23 23:14:44 -0800

    added a to do list consisting of picking up my dogs poop, and some other things

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// Arrays to keep track of each task's state //deleted
+// const taskTitles = []
+// const taskComplete = []
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: () => {
+    // Print the state of a task to the console in a nice readable way
+      console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+    },
+    completed: () => {
+      this.complete = true;
+    }
+  };
+  return task;
 }
+
+
+
+
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+
 
 // DRIVER CODE BELOW
+const task1 = newTask("Take moose for a walk and a 💩", "pick up his shits");
+const task2 = newTask("Go eat some ass", "😩");
+const tasks = [task1, task2];
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+task1.logState();                    // Clean Cat Litter has not been completed
+task1.completed();
+task1.logState(); // Clean Cat Litter has been completed
+
+console.log(tasks);
